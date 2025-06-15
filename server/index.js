6fab4887f8181e72bd9e4ccb3ecbeba2a9b39eb3
NwekeFavour <buchijunior90@gmail.com>
2025-06-15 02:02:27 +0100

    tests

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const allowedOrigins = [
-  // 'http://localhost:5173',
+  'http://localhost:5173',
   'https://echomorrow-beta.vercel.app'
 ];
 
